43ceca8e3efc4022ab8ead6bf097e008fc8a6107
Samuel Mapusao Borja <120239552+smb23@users.noreply.github.com>
2023-03-27 13:27:24 +0800

    Add files via upload

---
+++ myPHP/about.php
@@ -0,0 +1,14 @@
+
+<html><head></head><body>
+<table>
+    <tr>
+        <td>
+            <form id="finfo" method=POST>
+                <input type="submit" value="about">
+            </form> 
+        </td>
+    </tr>
+</table>
+<script type="text/javascript">alert("this is the about page");</script>
+<? echo $msg; ?>
+</body></html>
